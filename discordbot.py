14d5767e31db430236f9332173c78c760e52fa60
tl075 <46222287+tl075@users.noreply.github.com>
2020-11-26 15:11:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-async def aooniさん(ctx):
+async def aooni(ctx):
     await ctx.send('なんですか')
 
 
